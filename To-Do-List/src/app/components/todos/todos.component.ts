dfbd58085ed3dfc352ee6165abead1d11d980575
Michael <michaelli.sw.eng@gmail.com>
2021-03-10 22:46:01 -0800

    Added todo model

--- To-Do-List/src/app/components/todos/todos.component.ts
+++ To-Do-List/src/app/components/todos/todos.component.ts
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodosComponent implements OnInit {
 
-  constructor() { }
+	todos = [];
 
-  ngOnInit(): void {
-  }
+	constructor() { }
+
+	ngOnInit(): void {
+	}
 
 }
