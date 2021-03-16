19cf59d18effe4af917a2847d427d7e27eb17ccc
Michael <michaelli.sw.eng@gmail.com>
2021-03-15 22:51:11 -0700

    Added square component

--- tic-tac-toe/src/app/app.module.ts
+++ tic-tac-toe/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BoardComponent } from './board/board.component';
+import { SquareComponent } from './square/square.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BoardComponent
+    BoardComponent,
+    SquareComponent
   ],
   imports: [
     BrowserModule,
