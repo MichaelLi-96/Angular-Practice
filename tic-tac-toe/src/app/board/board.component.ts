a707f3a2f78bbbc442ab8c2a4fe9138d56ddeb86
Michael <michaelli.sw.eng@gmail.com>
2021-03-19 23:58:58 -0700

    Added checkWinner function

--- tic-tac-toe/src/app/board/board.component.ts
+++ tic-tac-toe/src/app/board/board.component.ts
@@ -7,9 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BoardComponent implements OnInit {
 
-	squares:any[] = [];
-	playerOnesTurn:boolean = true;  
-	winner:string = "";
+	squares: any[] = [];
+	playerOnesTurn: boolean = true;  
+	winner: string = "";
+	gameover: boolean = false;
 
   constructor() { }
 
@@ -21,8 +22,8 @@ export class BoardComponent implements OnInit {
   	return this.playerOnesTurn ? 'X' : 'O';
   }
 
-  handlePlayerMove(index:number):void {
-  	if(this.squares[index] !== "") {
+  handlePlayerMove(index: number):void {
+  	if(this.squares[index] !== "" || this.gameover) {
   		return;
   	}
   	if(this.playerOnesTurn) {
@@ -31,12 +32,34 @@ export class BoardComponent implements OnInit {
   	else {
   		this.squares[index] = 'O';
   	}
+  	this.checkWinner();
   	this.playerOnesTurn = !this.playerOnesTurn;
   }
 
-  handleRestartGame():void {
+  handleRestartGame(): void {
   	this.squares = Array(9).fill("");
   	this.playerOnesTurn = true;
   	this.winner = "";
+  }
+
+  checkWinner(): void {
+  	let winningMoves: number[][] = [
+  		[0, 1, 2],
+  		[3, 4, 5],
+  		[6, 7, 8],
+  		[0, 3, 6],
+  		[1, 4, 7],
+  		[2, 5, 8],
+  		[0, 4, 8],
+  		[2, 4, 6]
+  	];
+
+  	for(let winningMove of winningMoves) {
+  		let board = this.squares;
+  		if(board[winningMove[0]] === board[winningMove[1]] && board[winningMove[1]] === board[winningMove[2]]) {
+  			this.winner =this.playerOnesTurn ? "Player one wins" : "Player two wins";
+  			this.gameover = true;
+  		}
+  	}
   }
 }
